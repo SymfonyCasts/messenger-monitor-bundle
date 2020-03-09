57016fe3261b501b9d563164753489a873550777
Nicolas PHILIPPE <nikophil@gmail.com>
2020-03-09 15:34:15 +0100

    Store the retried saved message

---
+++ src/Storage/Doctrine/EventListener/SaveRetriedMessageListener.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SymfonyCasts\MessengerMonitorBundle\Storage\Doctrine\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use SymfonyCasts\MessengerMonitorBundle\FailedMessage\RetriedMessageEvent;
+use SymfonyCasts\MessengerMonitorBundle\Storage\Doctrine\Connection;
+use SymfonyCasts\MessengerMonitorBundle\Storage\Doctrine\StoredMessage;
+
+/**
+ * @internal
+ */
+final class SaveRetriedMessageListener implements EventSubscriberInterface
+{
+    private $doctrineConnection;
+
+    public function __construct(Connection $doctrineConnection)
+    {
+        $this->doctrineConnection = $doctrineConnection;
+    }
+
+    public function onMessageRetried(RetriedMessageEvent $retriedMessageEvent): void
+    {
+        $this->doctrineConnection->saveMessage(
+            new StoredMessage(
+                uuid_create(UUID_TYPE_RANDOM),
+                $retriedMessageEvent->getMessageUid(),
+                $retriedMessageEvent->getMessageClass(),
+                new \DateTimeImmutable()
+            )
+        );
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            RetriedMessageEvent::class => 'onMessageRetried',
+        ];
+    }
+}
